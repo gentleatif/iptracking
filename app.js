28ba91a739fa6ce2c313dfaa3897617763d1dd21
atifgriphhy <atif@griphhy.com>
2023-04-27 06:51:37 +0530

    handling ip

--- app.js
+++ app.js
@@ -1,29 +1,30 @@
-const express = require('express');
-const axios = require('axios');
+const express = require("express");
+const axios = require("axios");
 const app = express();
 
-const IPSTACK_API_KEY = 'd9711815ca3d9972bb0fda2a9811ed91';
+const IPSTACK_API_KEY = "d9711815ca3d9972bb0fda2a9811ed91";
 
-app.get('/ip-info', async (req, res) => {
+app.get("/ip-info", async (req, res) => {
   try {
-    const ipAddress =
-      req.headers['x-forwarded-for'] ||
-      req.connection.remoteAddress ||
-      req.socket.remoteAddress ||
-      req.connection.socket.remoteAddress;
+    var ip;
+    if (req.headers["x-forwarded-for"]) {
+      ip = req.headers["x-forwarded-for"].split(",")[0];
+    } else {
+      ip = req.ip;
+    }
+    console.log("client IP is *********************" + ip);
 
-    console.log('ipAddress: ', ipAddress);
     //  req.connection is deprecated, so we use alter
     const ipInfoResponse = await axios.get(
-      `https://api.ipstack.com/${ipAddress}?access_key=${IPSTACK_API_KEY}`
+      `https://api.ipstack.com/${ip}?access_key=${IPSTACK_API_KEY}`
     );
     res.json(ipInfoResponse.data);
   } catch (error) {
     console.error(error);
-    res.status(500).json({ error: 'Unable to retrieve IP information' });
+    res.status(500).json({ error: "Unable to retrieve IP information" });
   }
 });
 
 app.listen(3000, () => {
-  console.log('Server started on port 3000');
+  console.log("Server started on port 3000");
 });

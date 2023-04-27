2a5855a92e42e8a0fa808c79321c29e18f80ce97
atifgriphhy <atif@griphhy.com>
2023-04-27 08:47:29 +0530

    ds

--- app.js
+++ app.js
@@ -21,6 +21,10 @@ app.get("/ip-info", async (req, res) => {
   }
 });
 
+app.get("/", (req, res) => {
+  return res.send("Hello World!");
+});
+
 app.listen(3000, () => {
   console.log("Server started on port 3000");
 });
